2b01843fe2af9d5f9cf9eabf3f5d2734e552f00d
alwhali <alwhlai11@gmail.com>
2026-05-21 02:57:53 +0300

    do the design login page2 same of tutorial

--- lib/login_page2/login_page2.dart
+++ lib/login_page2/login_page2.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ui_login_pages/login_page2/app_colors2.dart';
+import 'package:animate_do/animate_do.dart';
 
 class LoginPage2 extends StatefulWidget {
   const LoginPage2({super.key});
@@ -23,6 +24,7 @@ class _LoginPage2State extends State<LoginPage2> {
 
     return Scaffold(
       backgroundColor: theme.primaryColor,
+
       appBar: AppBar(backgroundColor: theme.primaryColor, elevation: 0),
       body: SingleChildScrollView(
         child: SizedBox(
@@ -39,8 +41,17 @@ class _LoginPage2State extends State<LoginPage2> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Login", style: theme.textTheme.titleLarge),
-                    Text("Welcome Back", style: theme.textTheme.titleSmall),
+                    FadeInLeft(
+                      duration: Duration(milliseconds: 1000),
+                      child: Text("Login", style: theme.textTheme.titleLarge),
+                    ),
+                    FadeInLeft(
+                      duration: Duration(milliseconds: 1300),
+                      child: Text(
+                        "Welcome Back",
+                        style: theme.textTheme.titleSmall,
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -66,97 +77,138 @@ class _LoginPage2State extends State<LoginPage2> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Container(
-                      padding: EdgeInsets.only(
-                        left: 20,
-                        right: 20,
-                        top: 8,
-                        bottom: 8,
-                      ),
-                      width: size.width,
-                      height: 130,
-                      decoration: BoxDecoration(
-                        boxShadow: [BoxShadow(color: theme.primaryColor)],
-                        border: Border.all(color: theme.primaryColor),
-                        // borderRadius: BorderRadius.all(Radius.circular(20)),
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.white,
-                      ),
-                      child: Column(
-                        children: [
-                          TextField(
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Email or Phone number",
+                    FadeInLeft(
+                      duration: Duration(milliseconds: 1400),
+                      child: Container(
+                        padding: EdgeInsets.only(
+                          left: 20,
+                          right: 20,
+                          top: 8,
+                          bottom: 8,
+                        ),
+                        width: size.width,
+                        height: 130,
+                        decoration: BoxDecoration(
+                          // borderRadius: BorderRadius.all(Radius.circular(20)),
+                          // border: Border.all(color: theme.primaryColor),
+                          boxShadow: [
+                            BoxShadow(
+                              color: theme.primaryColor,
+                              blurRadius: 20,
+                              offset: Offset(0, 7),
                             ),
-                          ),
-                          Divider(),
-                          TextField(
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Password",
+                          ],
+
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white,
+                        ),
+
+                        child: Column(
+                          children: [
+                            TextField(
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Email or Phone number",
+                              ),
                             ),
-                          ),
-                        ],
+                            Divider(),
+                            TextField(
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Password",
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                     SizedBox(height: 40),
-                    Text(
-                      "Forgot Password?",
-                      style: theme.textTheme.labelMedium,
+                    FadeInLeft(
+                      duration: Duration(milliseconds: 1500),
+                      child: Text(
+                        "Forgot Password?",
+                        style: theme.textTheme.labelMedium,
+                      ),
                     ),
                     SizedBox(height: 40),
 
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text("Login ", style: theme.textTheme.titleSmall),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: theme.primaryColor,
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 60,
-                          vertical: 10,
+                    // FadeInLeft(
+                    //   duration: Duration(milliseconds: 1600),
+                    //   child: ElevatedButton(
+                    //     onPressed: () {},
+                    //     child: Text(
+                    //       "Login ",
+                    //       style: theme.textTheme.titleSmall,
+                    //     ),
+                    //     style: ElevatedButton.styleFrom(
+                    //       backgroundColor: theme.primaryColor,
+                    //       padding: EdgeInsets.symmetric(
+                    //         horizontal: 60,
+                    //         vertical: 10,
+                    //       ),
+                    //     ),
+                    //   ),
+                    // ),
+                    FadeInLeft(
+                      duration: Duration(milliseconds: 1600),
+                      child: MaterialButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Login ",
+                          style: theme.textTheme.titleSmall,
+                        ),
+                        height: 50,
+                        padding: EdgeInsets.symmetric(horizontal: 60),
+                        color: theme.primaryColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
                         ),
                       ),
                     ),
                     SizedBox(height: 40),
 
-                    Text(
-                      " Continue with ... ",
-                      style: theme.textTheme.labelMedium,
+                    FadeInLeft(
+                      duration: Duration(milliseconds: 1700),
+                      child: Text(
+                        " Continue with ... ",
+                        style: theme.textTheme.labelMedium,
+                      ),
                     ),
                     SizedBox(height: 40),
 
                     Row(
                       children: [
                         Expanded(
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            child: Text(
-                              "Facebook",
-                              style: theme.textTheme.titleSmall,
-                            ),
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.blue,
-                              padding: EdgeInsets.symmetric(
-                                horizontal: 20,
-                                vertical: 10,
+                          child: FadeInLeft(
+                            duration: Duration(milliseconds: 1800),
+                            child: MaterialButton(
+                              onPressed: () {},
+                              child: Text(
+                                "Facebook",
+                                style: theme.textTheme.titleSmall,
+                              ),
+                              height: 50,
+                              color: Colors.blue,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
                               ),
                             ),
                           ),
                         ),
                         SizedBox(width: 20),
                         Expanded(
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            child: Text(
-                              "Github",
-                              style: theme.textTheme.titleSmall,
-                            ),
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.black,
-                              padding: EdgeInsets.symmetric(
-                                horizontal: 20,
-                                vertical: 10,
+                          child: FadeInLeft(
+                            duration: Duration(milliseconds: 1900),
+                            child: MaterialButton(
+                              onPressed: () {},
+                              child: Text(
+                                "Github",
+                                style: theme.textTheme.titleSmall,
+                              ),
+                              height: 50,
+                              color: Colors.black,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
                               ),
                             ),
                           ),
